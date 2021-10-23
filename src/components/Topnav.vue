83a9be502dac640ad6ec110464f9ad8beb12d891
fishyabi <1027096592@qq.com>
2021-10-23 15:20:25 +0800

    完成网页整体布局

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,45 +1,75 @@
 <template>
     <div class="topnav">
-           <div class="logo" @click="ToggleAside">LOGO</div>
-           <ul class="menu">
-               <li>菜单1</li>
-               <li>菜单2</li>
-               </ul>           
-       </div>
+        <div class="logo">LOGO</div>
+        <ul class="menu">
+            <li>菜单1</li>
+            <li>菜单2</li>
+        </ul>
+        <span class="toggleAside" @click="ToggleAside"></span>
+    </div>
 </template>
 <script lang="ts">
 import { inject, Ref } from 'vue'
-export default{
+export default {
     setup() {
         const asideVisible = inject<Ref<boolean>>('asideVisible')
-        const ToggleAside = () =>{
-            asideVisible.value = !asideVisible.value 
+        const ToggleAside = () => {
+            asideVisible.value = !asideVisible.value
         }
-        console.log('Topnav的asideVisible'+asideVisible.value) 
-        return{ToggleAside}
+        return { ToggleAside }
     }
 }
 </script>
 <style lang="scss" scoped>
-.topnar{border: 1px solid red;}
-  
+.topnar {
+    border: 1px solid red;
+}
+
 .topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  position: relative;
-  z-index: 10;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
+    background: pink;
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+    padding: 16px;
+    position: relative;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 10;
+    justify-content: center;
+    align-items: center;
+    > .logo {
+        max-width: 6em;
+        margin-right: auto;
     }
-  }
+    > .menu {
+        display: flex;
+        white-space: nowrap;
+        flex-wrap: nowrap;
+        > li {
+            margin: 0 1em;
+        }
+    }
+    > .toggleAside {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+        display: none;
+    }
+    @media (max-width: 500px) {
+        > .menu {
+            display: none;
+        }
+        > .logo {
+            margin: 0 auto;
+        }
+        > .toggleAside {
+            display: block;
+        }
+    }
 }
 </style>
